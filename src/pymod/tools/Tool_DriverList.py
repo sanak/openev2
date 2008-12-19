b9d9dcc6d1e1e74e47142ba36667998282ffa40c
mariobe <mariobe@905608dc-911b-0410-879c-cfd6c6706665>
2008-12-19 02:48:49 +0000

    fix for no more gdal.GetDriverList code cleanup
    git-svn-id: https://openev.svn.sourceforge.net/svnroot/openev/trunk@64 905608dc-911b-0410-879c-cfd6c6706665

--- src/pymod/tools/Tool_DriverList.py
+++ src/pymod/tools/Tool_DriverList.py
@@ -28,68 +28,58 @@
 # Boston, MA 02111-1307, USA.
 ###############################################################################
 
-import gviewapp
-import gdal
+from gviewapp import Tool_GViewApp
+from osgeo import gdal
 import gvhtml
 import gtk
 
-class ToolDriverList(gviewapp.Tool_GViewApp):
-
-    def __init__(self, app=None,startpath=None):
-        gviewapp.Tool_GViewApp.__init__(self,app)
+class ToolDriverList(Tool_GViewApp):
+    def __init__(self, app=None, startpath=None):
+        Tool_GViewApp.__init__(self, app)
 
         self.init_menu()
 
         self.supported_list = []
-        self.unsupported_list=[]
-        self._official_support=[]
+        self.unsupported_list = []
+        self._official_support = []
 
-	for iDriver in gdal.GetDriverList():
-	    try:
-                next_list=[]
-                if iDriver.HelpTopic is not None:
-                    helpstr=iDriver.HelpTopic
-                else:
-                    helpstr=None
-                next_list.append(iDriver.LongName)
-                next_list.append(iDriver.ShortName)
-                mdata=iDriver.GetMetadata()
-                if mdata.has_key("DCAP_CREATECOPY"):
-                    next_list.append(mdata["DCAP_CREATECOPY"])
-                else:
-                    next_list.append("NO")
+        for i in range(gdal.GetDriverCount()):
+            iDriver = gdal.GetDriver(i)
+            next_list = []
+            if iDriver.HelpTopic:
+                helpstr = iDriver.HelpTopic
+            else:
+                helpstr = None
+            next_list.append(iDriver.LongName)
+            next_list.append(iDriver.ShortName)
 
-                if mdata.has_key("DMD_CREATIONDATATYPES"):
-                    next_list.append(mdata["DMD_CREATIONDATATYPES"])
-                else:
-                    next_list.append('Unknown')
+            mdata = iDriver.GetMetadata()
+            next_list.append(mdata.get("DCAP_CREATECOPY","NO"))
+            next_list.append(mdata.get("DMD_CREATIONDATATYPES",'Unknown'))
+            next_list.append(helpstr)
 
-                next_list.append(helpstr)
-                if iDriver.ShortName in self._official_support:
-                    self.supported_list.append(next_list)
-                else:
-                    self.unsupported_list.append(next_list)
-	    except KeyError:
-		pass        
+            if iDriver.ShortName in self._official_support:
+                self.supported_list.append(next_list)
+            else:
+                self.unsupported_list.append(next_list)
 
         self.init_dialog()
 
     def init_menu(self):
-        self.menu_entries.set_entry("Help/Formats...",1,self.driver_tool_cb)
-
+        self.menu_entries.set_entry("Help/Formats...", 1, self.driver_tool_cb)
 
     def init_dialog(self):
         self.dialog = gtk.Window()
         self.dialog.set_title('Available Image Formats')
         self.dialog.set_size_request(300,500)
         self.dialog.set_border_width(10) 
-        self.dialog.set_resizable(True)
-        self.tooltips=gtk.Tooltips()
+        self.dialog.set_resizable(False)
+        self.tooltips = gtk.Tooltips()
         self.button_dict={}
         # main shell 
-        mainshell = gtk.VBox(spacing=1, homogeneous=False)
+        mainshell = gtk.VBox(spacing=1)
         self.dialog.add(mainshell)
-        self.show_list=[]
+        self.show_list = []
         self.show_list.append(mainshell)
 
         #frame1=gtk.Frame('Supported')
@@ -114,56 +104,56 @@ class ToolDriverList(gviewapp.Tool_GViewApp):
         #    frame1.add(s_table)
         #    self.show_list.append(s_table)
 
-        num_us=len(self.unsupported_list)
-        frame2=gtk.Frame()
+        frame2 = gtk.Frame()
         pixel_scroll = gtk.ScrolledWindow()
+        pixel_scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         self.show_list.append(pixel_scroll)
         self.show_list.append(frame2)
         mainshell.pack_start(frame2)
         frame2.add(pixel_scroll)
-        num_us=len(self.unsupported_list)
-        if num_us > 0:
-            us_table = gtk.Table(num_us,3)
-            row=0
+        num_us = len(self.unsupported_list)
+        if num_us:
+            us_table = gtk.Table()
+            row = 0
             for fmt_list in self.unsupported_list:
-                clabel=gtk.Entry()
+                clabel = gtk.Entry()
                 clabel.set_editable(False)
                 clabel.set_text(fmt_list[0])
                 self.show_list.append(clabel)
-                self._make_tooltip(clabel,fmt_list)
-                us_table.attach(clabel,0,1,row,row+1)
+                self._make_tooltip(clabel, fmt_list)
+                us_table.attach(clabel, 0, 1, row, row+1)
                 if fmt_list[4] is not None:
-                    self.button_dict[fmt_list[1]]=gtk.Button('Help')
+                    self.button_dict[fmt_list[1]] = gtk.Button(stock=gtk.STOCK_HELP)
                     self.button_dict[fmt_list[1]].connect('clicked',self.help_clicked_cb,fmt_list[4])
-                    us_table.attach(self.button_dict[fmt_list[1]],1,2,row,row+1)
-                row=row+1
+                    us_table.attach(self.button_dict[fmt_list[1]], 1, 2, row, row+1)
+                row += 1
             pixel_scroll.add_with_viewport(us_table)
             self.show_list.append(us_table)
-        self.button_dict['close']=gtk.Button('Close')
-        self.button_dict['close'].connect('clicked',self.close)
-        mainshell.pack_start(self.button_dict['close'],expand=False)
+        self.button_dict['close'] = gtk.Button(stock=gtk.STOCK_CLOSE)
+        self.button_dict['close'].connect('clicked', self.close)
+        mainshell.pack_start(self.button_dict['close'], expand=False)
         self.show_list.append(self.button_dict['close'])
-        self.dialog.connect('delete-event',self.close)
+        self.dialog.connect('delete-event', self.close)
         for item in self.show_list:
             item.show()
 
-    def help_clicked_cb(self,but,topic):
+    def help_clicked_cb(self, but, topic):
         gvhtml.LaunchHTML(topic)
 
-    def driver_tool_cb(self,*args):
+    def driver_tool_cb(self, *args):
         for item in self.show_list:
             item.show()
         self.dialog.show_all()
-        self.dialog.window._raise()
+        self.dialog.present()
 
-    def _make_tooltip(self,clabel,info_list):
-        txt='Long Name: '+info_list[0]+'\n'
-        txt=txt+'Short Name: '+info_list[1]+'\n'
-        txt=txt+'Creation support: '+info_list[2]+'\n'
-        txt=txt+'Data types: '+info_list[3]
-        self.tooltips.set_tip(clabel,txt)
+    def _make_tooltip(self, clabel, info_list):
+        txt ='Long Name: '+info_list[0]+'\n'
+        txt += 'Short Name: '+info_list[1]+'\n'
+        txt += 'Creation support: '+info_list[2]+'\n'
+        txt += 'Data types: '+info_list[3]
+        self.tooltips.set_tip(clabel, txt)
 
-    def close(self,*args):
+    def close(self, *args):
         self.dialog.hide()
         return True
 
